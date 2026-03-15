7c3c6529f16109635ddbf272328cc7685282c7d2
Carlos Roberto Dantas Junior <crdantasjr@gmail.com>
2026-03-15 02:02:25 -0300

    update Main com Loop no Menu e Integração das novas Classes
    O arquivo Main foi atualizado para melhorar a interação com o usuário e foi feita a integração das novas classes.
    - Correção no Scanner do menu interativo,
    - Criação de um loop (do-while) que permite executar o programa várias vezes sem precisar reiniciar a aplicação e com isso a Inclusão da opção "0" no switch para encerrar o programa de forma controlada..
    - Adição de novas opções no menu: ContaBancaria, Arquivo, Aula e Avaliacao, junto da Integração dos métodos de cada classe dentro da estrutura switch-case.
    
    O objetivo dessas mudanças foi centralizar a execução das entidades do projeto em um único ponto de entrada, facilitando testes e demonstração das funcionalidades implementadas.

--- src/br/edu/fatecpg/tecprog/abstracao/view/Main.java
+++ src/br/edu/fatecpg/tecprog/abstracao/view/Main.java
@@ -7,72 +7,154 @@ import java.time.*;
 public class Main {
 
 	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
 		int op;
-		try (Scanner sc = new Scanner(System.in)) {
-			System.out.println("Escolha qual Classe deseja ver: " + "\n1.Produto" + "\n2.Curso" + "\n3.Rede Social");
-			op = sc.nextInt();
-		}
 
-		switch (op) {
-		case 1:
-			Produto newProduto = new Produto();
-			newProduto.nmProduto = "Notebook";
-			newProduto.idProduto = 100;
-			newProduto.qtEstoque = 50;
-			newProduto.valorProduto = 2499.90;
-			newProduto.categoriaProduto = "Eletrônicos";
+		do {
 
-			newProduto.consultarProduto();
-			System.out.println("-".repeat(35));
+			System.out.println("Escolha qual Classe deseja ver: " + "\n1.Produto" + "\n2.Curso" + "\n3.Rede Social"
+					+ "\n4.Conta Bancaria" + "\n5.Arquivo" + "\n6.Aula" + "\n7.Avaliação" + "\n0.Sair");
 
-			newProduto.venderProduto();
-			System.out.println("-".repeat(35));
+			op = scanner.nextInt();
 
-			newProduto.excluirProduto();
-			break;
+			switch (op) {
 
-		case 2:
-			Curso newCurso = new Curso();
+			case 1:
+				Produto newProduto = new Produto();
 
-			newCurso.nmCurso = "Desenvolvimento de Software e Multiplataforma";
-			newCurso.idCurso = 200;
-			newCurso.hrCurso = LocalTime.of(13, 10);
-			newCurso.materiaCurso = "Tecnicas de Programação I";
-			newCurso.nmProfessor = "Alessandro";
+				newProduto.nmProduto = "Notebook";
+				newProduto.idProduto = 100;
+				newProduto.qtEstoque = 50;
+				newProduto.valorProduto = 2499.90;
+				newProduto.categoriaProduto = "Eletrônicos";
 
-			newCurso.acessarCurso();
-			System.out.println("-".repeat(35));
+				newProduto.consultarProduto();
+				System.out.println("-".repeat(35));
 
-			newCurso.lecionarCurso();
-			System.out.println("-".repeat(35));
+				newProduto.venderProduto();
+				System.out.println("-".repeat(35));
 
-			newCurso.atualizarCurso();
+				newProduto.excluirProduto();
+				break;
 
-			break;
+			case 2:
+				Curso newCurso = new Curso();
 
-		case 3:
-			RedeSocial newRede = new RedeSocial();
+				newCurso.nmCurso = "Desenvolvimento de Software e Multiplataforma";
+				newCurso.idCurso = 200;
+				newCurso.hrCurso = LocalTime.of(13, 10);
+				newCurso.materiaCurso = "Tecnicas de Programação I";
+				newCurso.nmProfessor = "Alessandro";
 
-			newRede.nmRede = "Instagram";
-			newRede.idRedeSocial = 1;
-			newRede.qtUsuarios = 2000000;
-			newRede.algoritmo = "Recomendação por engajamento";
-			newRede.tipoConteudo = "Fotos e Vídeos";
+				newCurso.acessarCurso();
+				System.out.println("-".repeat(35));
 
-			newRede.criarPerfil();
-			System.out.println("-".repeat(35));
+				newCurso.lecionarCurso();
+				System.out.println("-".repeat(35));
 
-			newRede.enviarMensagem();
-			System.out.println("-".repeat(35));
+				newCurso.atualizarCurso();
+				break;
 
-			newRede.atualizarAlgoritmo();
-			break;
+			case 3:
+				RedeSocial newRede = new RedeSocial();
 
-		default:
-			System.out.println("Opção inválida!");
-			break;
-		}
+				newRede.nmRede = "Instagram";
+				newRede.idRedeSocial = 1;
+				newRede.qtUsuarios = 2000000;
+				newRede.algoritmo = "Recomendação por engajamento";
+				newRede.tipoConteudo = "Fotos e Vídeos";
+
+				newRede.criarPerfil();
+				System.out.println("-".repeat(35));
+
+				newRede.enviarMensagem();
+				System.out.println("-".repeat(35));
+
+				newRede.atualizarAlgoritmo();
+				break;
+
+			case 4:
+				ContaBancaria newConta = new ContaBancaria();
+
+				newConta.numeroConta = 12345;
+				newConta.numeroAgencia = 1001;
+				newConta.saldo = 5000.50;
+				newConta.tipoConta = "Corrente";
+				newConta.nomeCliente = "Carlos";
+
+				newConta.consultarSaldo();
+				System.out.println("-".repeat(35));
+
+				newConta.depositar();
+				System.out.println("-".repeat(35));
+
+				newConta.transferir();
+				break;
+
+			case 5:
+				Arquivo newArquivo = new Arquivo();
+
+				newArquivo.nomeArquivo = "Relatorio";
+				newArquivo.tipoArquivo = ".pdf";
+				newArquivo.tamanho = 2.5;
+				newArquivo.descricao = "Relatório de vendas";
+				newArquivo.proprietario = "Carlos";
+
+				newArquivo.visualizar();
+				System.out.println("-".repeat(35));
+
+				newArquivo.editar();
+				System.out.println("-".repeat(35));
+
+				newArquivo.compartilhar();
+				break;
+
+			case 6:
+				Aula newAula = new Aula();
+
+				newAula.nomeAula = "Introdução ao Java";
+				newAula.tipo = "Presencial";
+				newAula.material = "Slides e exercícios";
+				newAula.data = LocalDate.of(2025, 6, 20);
+				newAula.professor = "Alessandro";
+
+				newAula.lecionarAula();
+				System.out.println("-".repeat(35));
+
+				newAula.atualizarConteudo();
+				System.out.println("-".repeat(35));
+
+				newAula.registrarPresenca();
+				break;
+
+			case 7:
+				Avaliacao newAvaliacao = new Avaliacao();
+
+				newAvaliacao.nome = "Prova 1";
+				newAvaliacao.data = LocalDate.of(2025, 7, 10);
+				newAvaliacao.local = "Sala 10";
+				newAvaliacao.notaMaxima = 10;
+				newAvaliacao.disciplina = "Programação";
+
+				newAvaliacao.aplicar();
+				System.out.println("-".repeat(35));
+
+				newAvaliacao.corrigir();
+				System.out.println("-".repeat(35));
+
+				newAvaliacao.divulgarResultado();
+				break;
+
+			case 0:
+				System.out.println("Encerrando programa...");
+				break;
+
+			default:
+				System.out.println("Opção inválida!");
+			}
+
+		} while (op != 0);
 
 	}
-
 }
